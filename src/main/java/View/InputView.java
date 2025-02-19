3c65243058456a2aee716094879a4b01146104ed
wogus <ind07152@naver.com>
2025-02-19 17:25:54 +0900

    feat(view): 입력 기능 추가
    - 수정 입력 기능 추가
    - 메뉴 입력 기능 추가

--- src/main/java/View/InputView.java
+++ src/main/java/View/InputView.java
@@ -5,15 +5,43 @@ import java.util.Scanner;
 import util.Convertor;
 
 public class InputView {
+
+    public String getMenu(){
+        String output = "오늘은 "+ Convertor.dateFormattingForInput(LocalDateTime.now())+"입니다. 기능을 선택해 주세요";
+        System.out.println(output);
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("1. 출석 확인");
+        System.out.println("2. 출석 수정");
+        System.out.println("3. 크루별 출석 기록 확인");
+        System.out.println("4. 제적 위험자 확인");
+        System.out.println("Q. 종료");
+        return scanner.nextLine();
+    }
+
     public String getName() {
-        Scanner scan = new Scanner(System.in);
+        Scanner scanner = new Scanner(System.in);
         System.out.println("닉네임을 입력해 주세요.");
-        return scan.nextLine();
+        return scanner.nextLine();
     }
 
     public LocalDateTime getAttendanceTime() {
-        Scanner scan = new Scanner(System.in);
+        Scanner scanner = new Scanner(System.in);
         System.out.println("등교 시간을 입력해 주세요.");
-        return Convertor.changeToDate(scan.nextLine());
+        return Convertor.changeToDate(scanner.nextLine());
+    }
+
+    public String getEditName() {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("출석을 수정하려는 크루의 닉네임을 입력해 주세요.");
+        return scanner.nextLine();
+    }
+
+    public LocalDateTime getEditAttendanceTime() {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("수정하려는 날짜(일)를 입력해 주세요.");
+        String date = scanner.nextLine();
+        System.out.println("언제로 변경하겠습니까?");
+        String time = scanner.nextLine();
+        return Convertor.editDayOfMonth(date,time);
     }
 }
