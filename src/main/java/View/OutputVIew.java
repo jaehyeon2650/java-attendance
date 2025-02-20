b86f36394cc0636656e2d00054feb35ba3bded70
wogus <ind07152@naver.com>
2025-02-20 11:14:37 +0900

    refactor(view): OutputVIew 메서드 추출
    - OutputVIew 메서드 추출
    - LINE_SEPARATOR 추가
    - 출력 형식 간소화

--- src/main/java/View/OutputVIew.java
+++ src/main/java/View/OutputVIew.java
@@ -11,39 +11,24 @@ import util.Convertor;
 
 public class OutputVIew {
 
+    private static final String LINE_SEPARATOR = System.lineSeparator();
+
     public void printAttendanceConfirmation(LocalDateTime attendanceTime, String attendanceResult) {
-        String formattedOutput = Convertor.dateFormattingForOutput(attendanceTime);
-        formattedOutput += " (" + attendanceResult + ")";
-        System.out.println(formattedOutput);
+        String time = Convertor.dateFormattingForOutput(attendanceTime);
+        System.out.printf("%s (%s)\n",time,attendanceResult);
     }
 
-    public void printEditAttendance(LocalDateTime before, String beforeResult, LocalDateTime editTime,
+    public void printEditAttendance(LocalDateTime before, String beforeResult, LocalDateTime edit,
                                     String editResult) {
-        String formattedOutput = Convertor.dateFormattingForOutput(before);
-        formattedOutput += " (" + beforeResult + ") -> ";
-        String timeFormatOutput = Convertor.timeFormattingForOutput(editTime);
-        timeFormatOutput += " (" + editResult + ") 수정 완료!";
-        System.out.println(formattedOutput + timeFormatOutput);
+        String beforeTime = Convertor.dateFormattingForOutput(before);
+        String editTime = Convertor.timeFormattingForOutput(edit);
+        System.out.printf("%s (%s) -> %s (%s) 수정 완료!\n",beforeTime,beforeResult,editTime,editResult);
     }
 
     public void printHistories(HistoriesDto historiesDto){
-        System.out.printf("이번 달 %s의 출석 기록입니다.\n",historiesDto.username());
-        for (HistoryDto history : historiesDto.histories()) {
-            String formattedOutput = Convertor.dateFormattingForOutput(history.time());
-            formattedOutput += " (" + history.attendanceResult()+ ")";
-            System.out.println(formattedOutput);
-        }
-        Map<String, Integer> result = historiesDto.attendanceAllResult();
-        System.out.println();
-        System.out.printf("출석: %d회\n",result.getOrDefault("출석",0));
-        System.out.printf("지각: %d회\n",result.getOrDefault("지각",0));
-        System.out.printf("결석: %d회\n",result.getOrDefault("결석",0));
-        String classifyResult =historiesDto.classifyAbsenceLevel();
-        System.out.println();
-        if(!classifyResult .equals("정상")){
-            System.out.printf("%s 대상자 입니다.\n",classifyResult);
-        }
-        System.out.println();
+        printAttendanceHistory(historiesDto);
+        printAttendanceAllResult(historiesDto);
+        printUserAttendanceResult(historiesDto);
     }
 
     public void printDangerous(AbsenceCrewsDto crewsDto){
@@ -56,7 +41,32 @@ public class OutputVIew {
                     results.getOrDefault("지각",0),
                     crew.getClassifyAbsenceLevel());
         });
-        System.out.println();
+        System.out.print(LINE_SEPARATOR);
+    }
+
+    private void printAttendanceHistory(HistoriesDto historiesDto) {
+        System.out.printf("이번 달 %s의 출석 기록입니다.\n", historiesDto.username());
+        for (HistoryDto history : historiesDto.histories()) {
+            String time = Convertor.dateFormattingForOutput(history.time());
+            System.out.printf("%s (%s)\n",time,history.attendanceResult());
+        }
+    }
+
+    private void printUserAttendanceResult(HistoriesDto historiesDto) {
+        String classifyResult = historiesDto.classifyAbsenceLevel();
+        if(!classifyResult .equals("정상")){
+            System.out.printf("%s 대상자 입니다.\n",classifyResult);
+        }
+        System.out.print(LINE_SEPARATOR);
+    }
+
+    private void printAttendanceAllResult(HistoriesDto historiesDto) {
+        Map<String, Integer> result = historiesDto.attendanceAllResult();
+        System.out.print(LINE_SEPARATOR);
+        System.out.printf("출석: %d회\n", result.getOrDefault("출석", 0));
+        System.out.printf("지각: %d회\n", result.getOrDefault("지각", 0));
+        System.out.printf("결석: %d회\n", result.getOrDefault("결석", 0));
+        System.out.print(LINE_SEPARATOR);
     }
 }
 
