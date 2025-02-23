eec915bf6a6a4f8d6680c44a111e816436b0458c
wogus <ind07152@naver.com>
2025-02-23 22:03:03 +0900

    refactor(AttendanceHistory): AttendanceHistory 리펙토링
    - 메서드 위치 변경(getter 메서드 하단으로 이동)
    - 맴버 변수 위치 변경

--- src/main/java/domain/AttendanceHistory.java
+++ src/main/java/domain/AttendanceHistory.java
@@ -3,14 +3,21 @@ package domain;
 import java.time.LocalDateTime;
 
 public class AttendanceHistory implements Comparable<AttendanceHistory> {
-    public final static int ABSENT_DEFAULT_HOUR = 23;
-    public final static int ABSENT_DEFAULT_MINUTE = 59;
     private final LocalDateTime attendanceTime;
     private final AttendanceResult attendanceResult;
+
+    public final static int ABSENT_DEFAULT_HOUR = 23;
+    public final static int ABSENT_DEFAULT_MINUTE = 59;
+
 
     public AttendanceHistory(LocalDateTime attendanceTime) {
         this.attendanceTime = attendanceTime;
         this.attendanceResult = DailyAttendanceTime.getDailyAttendanceResult(attendanceTime);
+    }
+
+    public boolean isBeforeHistory(LocalDateTime time) {
+        LocalDateTime standardTime = LocalDateTime.of(time.getYear(), time.getMonthValue(), time.getDayOfMonth(), 0, 0);
+        return attendanceTime.isBefore(standardTime);
     }
 
     public LocalDateTime getAttendanceTime() {
@@ -19,11 +26,6 @@ public class AttendanceHistory implements Comparable<AttendanceHistory> {
 
     public String getAttendanceResult() {
         return attendanceResult.getResult();
-    }
-
-    public boolean isBeforeHistory(LocalDateTime time) {
-        LocalDateTime standardTime = LocalDateTime.of(time.getYear(), time.getMonthValue(), time.getDayOfMonth(), 0, 0);
-        return attendanceTime.isBefore(standardTime);
     }
 
     @Override
