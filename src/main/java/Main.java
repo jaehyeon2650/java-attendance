aa343c3fa2d510c8d86dd3e551104badebdc0f79
wogus <ind07152@naver.com>
2025-02-26 19:37:06 +0900

    chore: 기존 코드 전부 삭제

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,10 +1,4 @@
-import configure.Configure;
-import controller.AttendanceController;
-
 public class Main {
     public static void main(String[] args) {
-        Configure configure = new Configure();
-        AttendanceController attendanceController = configure.attendanceController();
-        attendanceController.start();
     }
 }
