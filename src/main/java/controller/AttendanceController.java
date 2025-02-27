c1a7a6bb850efda8ec1749f329f8b42c7b44efab
wogus <ind07152@naver.com>
2025-02-27 17:03:41 +0900

    feat(controller): AttendanceController 수정 기능 추가

--- src/main/java/controller/AttendanceController.java
+++ src/main/java/controller/AttendanceController.java
@@ -1,8 +1,12 @@
 package controller;
 
+import domain.AttendanceHistory;
+import domain.AttendanceHistory.AttendanceValidator;
 import domain.Crews;
+import dto.EditResponseDto;
 import java.time.LocalDate;
 import java.time.LocalDateTime;
+import java.time.LocalTime;
 import java.util.List;
 import java.util.Map;
 import util.FileReader;
@@ -23,11 +27,24 @@ public class AttendanceController {
         Crews crews = initializeCrews();
         String selection = inputView.getSelection();
         if(selection.equals("1")){
+            LocalDate now = LocalDate.now();
+            LocalDate changedDate = LocalDate.of(2024,12,now.getDayOfMonth());
+            AttendanceValidator.validateAttendanceDate(changedDate);
             String username = inputView.getUsernameForAddHistory();
             crews.validateHasCrew(username);
             LocalDateTime attendanceTime = inputView.getAttendanceTime();
             String result = crews.addAttendanceHistory(username, attendanceTime);
             outputView.printAddAttendanceResult(attendanceTime,result);
+        }else if(selection.equals("2")){
+            String username = inputView.getUsernameForEditHistory();
+            crews.validateHasCrew(username);
+            LocalDate attendDate = inputView.getEditDay();
+            AttendanceValidator.validateAttendanceDate(attendDate);
+            AttendanceHistory beforeHistory = crews.findAttendanceHistory(username, attendDate);
+            LocalTime attendTime = inputView.getEditTime();
+            LocalDateTime attendanceTime = LocalDateTime.of(attendDate,attendTime);
+            String result = crews.editAttendanceHistory(username, attendanceTime);
+            outputView.printEditResult(EditResponseDto.of(beforeHistory,attendanceTime,result));
         }
     }
 
