de1cf1213ec031259399e7554ae8cc1e035065c9
wogus <ind07152@naver.com>
2025-02-28 16:52:42 +0900

    feat(dto): 제적 위험자 dto 추가

---
+++ src/main/java/dto/ExpulsionCrewDto.java
@@ -0,0 +1,14 @@
+package dto;
+
+import domain.AttendanceAnalyze;
+
+public record ExpulsionCrewDto(
+        String username,
+        int lateCount,
+        int absenceCount,
+        String result
+) {
+    public static ExpulsionCrewDto of(String username, AttendanceAnalyze analyze){
+        return new ExpulsionCrewDto(username, analyze.getLateCount(), analyze.getAbsenceCount(), analyze.getAttendanceStatus().getStatus());
+    }
+}
---
+++ src/main/java/dto/ExpulsionCrewsDto.java
@@ -0,0 +1,16 @@
+package dto;
+
+import domain.AttendanceAnalyze;
+import java.util.List;
+import java.util.Map;
+
+public record ExpulsionCrewsDto(
+        List<ExpulsionCrewDto> crews
+) {
+    public static ExpulsionCrewsDto from(Map<String, AttendanceAnalyze> crewsInfo){
+        List<ExpulsionCrewDto> crews = crewsInfo.entrySet().stream()
+                .map(entry -> ExpulsionCrewDto.of(entry.getKey(), entry.getValue()))
+                .toList();
+        return new ExpulsionCrewsDto(crews);
+    }
+}
