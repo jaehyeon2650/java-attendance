32ff7a3f270711d6e3fb4beff250c6cf9978231d
wogus <ind07152@naver.com>
2025-02-28 15:35:59 +0900

    feat(view): 이달의 출석 기록 출력 기능 추가

--- src/main/java/view/OutputView.java
+++ src/main/java/view/OutputView.java
@@ -1,9 +1,13 @@
 package view;
 
+import domain.AttendanceStatus;
+import dto.AttendanceHistoriesDto;
+import dto.AttendanceHistoryDto;
 import dto.EditResponseDto;
 import java.time.LocalDateTime;
 import java.time.LocalTime;
 import java.time.format.DateTimeFormatter;
+import java.util.List;
 import java.util.Optional;
 
 public class OutputView {
@@ -27,6 +31,30 @@ public class OutputView {
         String afterResult = editResponseDto.afterResult();
         System.out.printf("%s %s (%s) -> %s (%s) 수정 완료!\n", beforeDate, beforeTime, beforeResult, afterTime,
                 afterResult);
+    }
+
+    public void printAttendanceHistories(AttendanceHistoriesDto historiesDto) {
+        System.out.printf("이번 달 %s의 출석 기록입니다.\n", historiesDto.username());
+        System.out.println();
+        List<AttendanceHistoryDto> histories = historiesDto.histories();
+        for (AttendanceHistoryDto history : histories) {
+            DateTimeFormatter dateFormatter = DateTimeFormatter.ofPattern("M월 d일 EEEE");
+            String date = dateFormatter.format(history.attendanceDate());
+            String time = "--:--";
+            if (history.attendanceTime().isPresent()) {
+                time = getTime(history.attendanceTime().get());
+            }
+            System.out.printf("%s %s (%s)\n", date, time, history.result());
+        }
+        System.out.println();
+        System.out.printf("출석: %d회\n", historiesDto.attendanceCount());
+        System.out.printf("지각: %d회\n", historiesDto.lateCount());
+        System.out.printf("결석: %d회\n", historiesDto.absenceCount());
+        System.out.println();
+
+        if (!historiesDto.status().equals(AttendanceStatus.NORMAL.getStatus())) {
+            System.out.printf("%s 대상자입니다.", historiesDto.status());
+        }
     }
 
     private String getTime(LocalTime beforeTime) {
