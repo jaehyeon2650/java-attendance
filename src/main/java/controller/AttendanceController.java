43080e89293314c1d83e8cb3bdfc7680afbd644d
wogus <ind07152@naver.com>
2025-02-28 16:58:10 +0900

    feat(controller): 재입력 기능 추가
    - 예외 발생 시 메뉴 재입력
    - 예외 발생 시 메시지 출력

--- src/main/java/controller/AttendanceController.java
+++ src/main/java/controller/AttendanceController.java
@@ -31,38 +31,46 @@ public class AttendanceController {
     public void start() {
         Crews crews = initializeCrews();
         String selection = inputView.getSelection();
-        if(selection.equals("1")){
-            LocalDate now = LocalDate.now();
-            LocalDate changedDate = LocalDate.of(2024,12,now.getDayOfMonth());
-            AttendanceValidator.validateAttendanceDate(changedDate);
-            String username = inputView.getUsername();
-            crews.validateHasCrew(username);
-            LocalDateTime attendanceTime = inputView.getAttendanceTime();
-            AttendanceResult result = crews.addAttendanceHistory(username, attendanceTime);
-            outputView.printAddAttendanceResult(attendanceTime,result.getResult());
-        }else if(selection.equals("2")){
-            String username = inputView.getUsernameForEditHistory();
-            crews.validateHasCrew(username);
-            LocalDate attendDate = inputView.getEditDay();
-            AttendanceValidator.validateAttendanceDate(attendDate);
-            AttendanceHistory beforeHistory = crews.findAttendanceHistory(username, attendDate);
-            LocalTime attendTime = inputView.getEditTime();
-            LocalDateTime attendanceTime = LocalDateTime.of(attendDate,attendTime);
-            AttendanceResult result = crews.editAttendanceHistory(username, attendanceTime);
-            outputView.printEditResult(EditResponseDto.of(beforeHistory,attendanceTime,result));
-        }else if(selection.equals("3")) {
-            String username = inputView.getUsername();
-            LocalDate now = LocalDate.now();
-            LocalDate changed = LocalDate.of(2024, 12, now.getDayOfMonth());
-            List<AttendanceHistory> attendanceHistories = crews.findAttendanceHistories(username, changed);
-            AttendanceAnalyze attendanceAnalyze = crews.getAttendanceAnalyze(username, changed);
-            outputView.printAttendanceHistories(
-                    AttendanceHistoriesDto.of(username, attendanceHistories, attendanceAnalyze));
-        } else if(selection.equals("4")){
-            LocalDate now = LocalDate.now();
-            LocalDate changed = LocalDate.of(2024, 12, now.getDayOfMonth());
-            Map<String, AttendanceAnalyze> expulsionCrews = crews.findExpulsionCrews(changed);
-            outputView.printExpulsionCrews(ExpulsionCrewsDto.from(expulsionCrews));
+        while(!selection.equals("Q")){
+            try{
+                if(selection.equals("1")){
+                    LocalDate now = LocalDate.now();
+                    LocalDate changedDate = LocalDate.of(2024,12,now.getDayOfMonth());
+                    AttendanceValidator.validateAttendanceDate(changedDate);
+                    String username = inputView.getUsername();
+                    crews.validateHasCrew(username);
+                    LocalDateTime attendanceTime = inputView.getAttendanceTime();
+                    AttendanceResult result = crews.addAttendanceHistory(username, attendanceTime);
+                    outputView.printAddAttendanceResult(attendanceTime,result.getResult());
+                }else if(selection.equals("2")){
+                    String username = inputView.getUsernameForEditHistory();
+                    crews.validateHasCrew(username);
+                    LocalDate attendDate = inputView.getEditDay();
+                    AttendanceValidator.validateAttendanceDate(attendDate);
+                    AttendanceHistory beforeHistory = crews.findAttendanceHistory(username, attendDate);
+                    LocalTime attendTime = inputView.getEditTime();
+                    LocalDateTime attendanceTime = LocalDateTime.of(attendDate,attendTime);
+                    AttendanceResult result = crews.editAttendanceHistory(username, attendanceTime);
+                    outputView.printEditResult(EditResponseDto.of(beforeHistory,attendanceTime,result));
+                }else if(selection.equals("3")) {
+                    String username = inputView.getUsername();
+                    LocalDate now = LocalDate.now();
+                    LocalDate changed = LocalDate.of(2024, 12, now.getDayOfMonth());
+                    List<AttendanceHistory> attendanceHistories = crews.findAttendanceHistories(username, changed);
+                    AttendanceAnalyze attendanceAnalyze = crews.getAttendanceAnalyze(username, changed);
+                    outputView.printAttendanceHistories(
+                            AttendanceHistoriesDto.of(username, attendanceHistories, attendanceAnalyze));
+                } else if(selection.equals("4")){
+                    LocalDate now = LocalDate.now();
+                    LocalDate changed = LocalDate.of(2024, 12, now.getDayOfMonth());
+                    Map<String, AttendanceAnalyze> expulsionCrews = crews.findExpulsionCrews(changed);
+                    outputView.printExpulsionCrews(ExpulsionCrewsDto.from(expulsionCrews));
+                }
+            }catch(IllegalArgumentException e){
+                outputView.printErrorMessage(e.getMessage());
+            }finally {
+                selection = inputView.getSelection();
+            }
         }
     }
 
